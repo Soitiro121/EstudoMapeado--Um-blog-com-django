17a51391b8641f038e0ae045e238c96bf57d2c7e
Douglas Oliveira <douglas_oliveira@usp.br>
2023-11-15 12:44:39 -0300

    inserção das view dos modelos

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
+# Fazendo a mudança do nome na pagina do admin
     class Meta:
         verbose_name_plural = "categories"
 
@@ -17,6 +18,7 @@ class Post(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField("Category", related_name="posts")
 
+# novamente mudança do nome para ficar mais facil a referenciação
     def __str__(self):
         return self.title
 
@@ -26,5 +28,6 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey("Post", on_delete=models.CASCADE)
 
+# apenas mudança de nome , com indexação
     def __str__(self):
         return f"{self.author} on '{self.post}'"
