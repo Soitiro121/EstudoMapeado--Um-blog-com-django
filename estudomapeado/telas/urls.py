af4bee8c5fb04333ce0333d89f94a2f4734db73a
Douglas Oliveira <douglas_oliveira@usp.br>
2023-11-25 16:48:13 -0300

    adição de template altera_senha

--- estudomapeado/telas/urls.py
+++ estudomapeado/telas/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from django.views.generic.base import RedirectView
 from . import views
 from django.contrib.auth import views as auth_views
-
+from .views import altera_senha
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('cria_conta/', views.cria_conta, name='cria_conta'),
     path('student/home/', views.student_home, name='student_home'),
     path('teacher/home/', views.teacher_home, name='teacher_home'),
     path('', RedirectView.as_view(url='/login/', permanent=True)),  # Redirecionamento para login
+    path('altera_senha/', altera_senha, name='altera_senha'),
 ]
