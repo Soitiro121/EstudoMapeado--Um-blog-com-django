700e9fa5daf3d7670ca0fe5ac5b00a0ad9b73a63
Douglas Oliveira <douglas_oliveira@usp.br>
2023-11-25 22:59:39 -0300

    conserto do template termos de uso

--- estudomapeado/telas/urls.py
+++ estudomapeado/telas/urls.py
@@ -3,6 +3,7 @@ from django.views.generic.base import RedirectView
 from . import views
 from django.contrib.auth import views as auth_views
 from .views import *
+
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('cria_conta/', views.cria_conta, name='cria_conta'),
